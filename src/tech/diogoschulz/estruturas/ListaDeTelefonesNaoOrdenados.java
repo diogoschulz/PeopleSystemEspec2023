d7ba4faba623da6414bf2a30eb191a526b16d274
Diogo Moreira <diogosmoreira@hotmail.com>
2023-11-25 10:50:41 -0300

    Adicionar capacidade

--- src/tech/diogoschulz/estruturas/ListaDeTelefonesNaoOrdenados.java
+++ src/tech/diogoschulz/estruturas/ListaDeTelefonesNaoOrdenados.java
@@ -12,22 +12,55 @@ public class ListaDeTelefonesNaoOrdenados {
 		public ListaDeTelefonesNaoOrdenados() {
 			this.telefones = new Telefone[CAPACIDADE_PADRAO];
 			this.tamanho = 0;
-			indexTel = -1;
+			//indexTel = -1;
 			
 		}
-		
+		//verificador de vazio
 		public boolean isEmpty() {
 			return this.tamanho == 0;
 		}
 		
+		private void garantirCapacidade( ) {
+			int novaCapacidade = telefones.length * 2;
+			Telefone[] listaTelefones = new Telefone[novaCapacidade];
+			for (int i = 1; i < tamanho; i++) {
+				listaTelefones[i] = telefones[i];
+			}
+			
+			telefones = listaTelefones;
+			
+		}
+		
+		
+		//adicionar um telefone
 		public void addTelefone(Telefone telefone) {
 			//lenght ele pega a capacidade atual em tempo de execução,
 			//caso a CAPACIDADE_PADRAO seja alterada
 			if(tamanho < telefones.length) {
-				telefones[indexTel++] = telefone;
-				indexTel = indexTel +1;
+				//o tamanho já é index + 1, portanto vamos usar o tamanho
+				//telefones[indexTel++] = telefone;
+				//indexTel = indexTel +1;
+				telefones[tamanho] = telefone;
+				//incremento do tamanho
 				this.tamanho = tamanho + 1;
 			}
+		}
+			
+			//adicionar um telefone VIDA LOKA
+			public void addTelefoneNoSQL(Telefone telefone) {
+				//lenght ele pega a capacidade atual em tempo de execução,
+				//caso a CAPACIDADE_PADRAO seja alterada
+				if(tamanho == telefones.length) {
+					//se o vetor estiver cheio, aumente a sua capacidade
+					garantirCapacidade();
+					
+				}
+				
+				telefones[tamanho++] = telefone;
+				// a linha de baixo representa o "++" acima, posso então, apagar o "++" e usar ela
+				//this.tamanho = tamanho + 1;
+				
+				
 			
 		}
 		
